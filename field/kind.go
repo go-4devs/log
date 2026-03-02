811b78e1d2d951caf1d116accfee4d5e85664410
andrey <andrey@4devs.io>
2026-03-02 16:43:35 +0300

    update unmarshal text

--- field/kind.go
+++ field/kind.go
@@ -37,44 +37,76 @@ func (l Kind) MarshalText() ([]byte, error) {
 	return []byte(l.String()), nil
 }
 
-//nolint:gocyclo,cyclop
+//nolint:gocyclo,cyclop,funlen
 func (l *Kind) UnmarshalText(in []byte) error {
 	switch string(in) {
 	case KindAny.String():
 		*l = KindAny
+
+		return nil
 	case KindArray.String():
 		*l = KindArray
+
+		return nil
 	case KindNil.String():
 		*l = KindNil
+
+		return nil
 	case KindString.String():
 		*l = KindString
+
+		return nil
 	case KindBool.String():
 		*l = KindBool
+
+		return nil
 	case KindInt64.String():
 		*l = KindInt64
+
+		return nil
 	case KindUint64.String():
 		*l = KindUint64
+
+		return nil
 	case KindFloat32.String():
 		*l = KindFloat32
+
+		return nil
 	case KindFloat64.String():
 		*l = KindFloat64
+
+		return nil
 	case KindComplex128.String():
 		*l = KindComplex128
+
+		return nil
 	case KindBinary.String():
 		*l = KindBinary
+
+		return nil
 	case KindDuration.String():
 		*l = KindDuration
+
+		return nil
 	case KindTime.String():
 		*l = KindTime
+
+		return nil
 	case KindError.String():
 		*l = KindError
+
+		return nil
 	case KindGroup.String():
 		*l = KindGroup
+
+		return nil
 	case KindClosure.String():
 		*l = KindClosure
-	}
 
-	return fmt.Errorf("%w:filed(%v)", ErrUndefined, string(in))
+		return nil
+	default:
+		return fmt.Errorf("%w:filed(%v)", ErrUndefined, string(in))
+	}
 }
 
 func (l Kind) MarshalBinary() ([]byte, error) {
